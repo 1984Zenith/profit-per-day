5715b0b13eab073d07f71b95b55f345cd1e8d102
1984Zenith <90709483+1984Zenith@users.noreply.github.com>
2020-06-15 13:33:46 +0000

    Edited readme.md via GitHub

--- CoinPayment/CoinPayment/CoinApi.cs
+++ CoinPayment/CoinPayment/CoinApi.cs
@@ -102,3 +102,5 @@ namespace CoinPayment
                 catch
                 {
                     var data = JsonConvert.DeserializeObject<TransactionException>(str);
+                    throw data;
+                }
